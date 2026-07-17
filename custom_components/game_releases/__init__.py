e645abcd21c7bf4cc02cf570d9fbb0e2c37fb60c
Luis Antonio Garcia Roque <78177901+toonymak1993@users.noreply.github.com>
2026-07-17 01:09:51 +0100

    Fix HACS validation metadata

--- custom_components/game_releases/__init__.py
+++ custom_components/game_releases/__init__.py
@@ -7,9 +7,12 @@ from pathlib import Path
 from homeassistant.components.http import StaticPathConfig
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
-from .const import CARD_URL, PLATFORMS
+from .const import CARD_URL, DOMAIN, PLATFORMS
 from .coordinator import GameReleasesCoordinator
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
